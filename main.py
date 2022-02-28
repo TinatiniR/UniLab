e2f71a303d799b06e699b5b83409f35664439240
Tinatini <tinatini.rostomashvili.1@iliauni.edu.ge>
2022-02-28 16:49:03 +0400

    ganvaaxle main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import sqlite3
+con=sqlite3.connect('identifier.sqlite', check_same_thread=False)
+cur=con.cursor()
 
-def print_hi(name):
 
-    print(f'Hi, {name}')
+app = Flask(__name__)
+api = Api(app)
 
+
+class Students(Resource):
+   def get(self, student_id):
+       cur.execute("SELECT * FROM students where id=?", (student_id,))
+       return  cur.fetchone()
+
+
+api.add_resource(Students, '/read/<int:student_id>')
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    app.run(debug=True, port=8080)
